d5214f978ddc0610abf271ed43216e749480dc05
AL-2713 <alexholland135@gmail.com>
2026-04-17 22:34:27 -0500

    Create medals.sql

---
+++ sql/medals.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS "medals" (
+	"app_id"	TEXT,
+	"medal_id"	INTEGER,
+	"medal_name"	TEXT,
+	"medal_description"	TEXT,
+	"medal_icon"	TEXT,
+	"medal_difficulty"	INTEGER,
+	"medal_value"	INTEGER,
+	"secret"	INTEGER
+)
